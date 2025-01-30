533cf2557b93f202daf0f7e8ed46e31c1489bc90
Vikas Yadav <128998971+VikasYadav711@users.noreply.github.com>
2025-01-30 16:55:07 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,16 +1,26 @@
 package com.driver;
 
 class Product{
-  public int product(int x, int y) {}
+  public int product(int x, int y) {
+    return x+y;
+  }
 
-  public int product(int x, int y, int z) {}
+  public int product(int x, int y, int z) {
+    return x+y+z;
+  }
 
-  public double product(double x, double y) {}
+  public double product(double x, double y) {
+    return x+y;
+  }
 }
 
 public class Main {
+  public static void Main(String args[])
+  {
   Product p=new Product();
-  p.product(10,20);
-  p.product(10,20,30);
-  p.product(5.5, 6.5);
+  
+  System.out.println(p.product(10,20));
+  System.out.println(p.product(10,20,30));
+  System.out.println(p.product(5.5, 6.5));
+}
 }
